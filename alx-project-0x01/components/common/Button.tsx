83685950096480e1de10cee64d0bee76a7b7c1e1
MarouaneCH30 <143810252+MarouaneCH30@users.noreply.github.com>
2025-06-01 02:32:35 +0100

    Delete alx-project-0x01 directory

--- alx-project-0x01/components/common/Button.tsx
+++
@@ -1,8 +0,0 @@
-const Button: React.FC = () => {
-    return (
-        <button>
-
-        </button>
-    )
-}
-export default Button;
--- alx-project-0x01/components/layout/Footer.tsx
+++
@@ -1,8 +0,0 @@
- const Footer: React.FC = () => {
-  return (
-    <footer>
-      <p>&copy; MyApp. All rights reserved.</p>
-    </footer>
-  );
-}
-export default Footer;
